155980cfb7f9d9d870e82543395c3b96346fa1e3
just-tracker <yanislav.2k2@gmail.com>
2024-12-05 12:18:24 +0200

    Add prittier format and more libraries

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,19 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from 'express'
 
-const app = express();
+const app = express()
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
 
 // Routes
 
 // Error Handling Middleware
 app.use((err: Error, req: Request, res: Response) => {
-  res.status(500).json({ message: err.message });
-});
+  res.status(500).json({ message: err.message })
+})
 
 // Start Server
-const PORT = 3000;
+const PORT = 3000
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+  console.log(`Server running on http://localhost:${PORT}`)
+})
